aae1f933ed0e85c2c26c3f7b8f533937123ca99b
Shivam Singh <shivam.singh.connect@gmail.com>
2023-09-01 16:43:03 +0200

    code fixes with Feedback from Dzianis

---
+++ src/main/java/com/xm/cryptoservice/exception/CryptoserviceException.java
@@ -0,0 +1,10 @@
+package com.xm.cryptoservice.exception;
+
+public class CryptoserviceException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CryptoserviceException(String msg) {
+		super(msg);
+	}
+}
